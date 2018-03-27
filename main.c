058fd0daa296d11a20cc5a1a242b5e795a97be46
William W Wold <wilcoze@gmail.com>
2018-03-27 01:49:15 -0400

    implicit unfuckupable destructors in C here we come

--- main.c
+++ main.c
@@ -2,32 +2,62 @@
 #include <stdlib.h>
 #include "define_EVIL/define_EVIL.h"
 
-#define COMMA() ,
+struct _Scope
+{
+	void * data;
+	void (*drop)(void*);
+	struct _Scope* next;
+};
 
-#define func_INTERNAL(type, name, arg_types_and_types, arg_names) \
-	type name##_WRAPPED(EXPAND arg_types_and_types, void *); \
-	type name arg_types_and_types \
+void * make_obj(void * data, void(*drop)(void*), struct _Scope ** scope)
+{
+	struct _Scope * tmp = *scope;
+	*scope = malloc(sizeof(struct _Scope));
+	(*scope)->data = data;
+	(*scope)->drop = drop;
+	(*scope)->next = tmp;
+	return data;
+}
+
+#define COMMA() (),
+
+#define CHECK_VOID_void
+#define SPEW_A_THING(...) __VA_ARGS__
+#define SPEW_NOTHING(...)
+
+#define func_INTERNAL(type, name, a, b, c) \
+	type name##_WRAPPED(EXPAND b struct _Scope **); \
+	type name c \
 	{ \
-		type ret = name##_WRAPPED(EXPAND arg_names, NULL); \
-		return ret; \
+		struct _Scope* scope = NULL; \
+		EXPAND_CAT(SPEW_, CHECK_IF_THING(CHECK_VOID_##type)) (type ret =) name##_WRAPPED(EXPAND a &scope); \
+		while (scope) \
+		{ \
+			scope->drop(scope->data); \
+			scope = scope->next; \
+		} \
+		EXPAND_CAT(SPEW_, CHECK_IF_THING(CHECK_VOID_##type)) (return ret;) \
 	} \
-	type name##_WRAPPED(EXPAND arg_types_and_types, void * SCOPE) \
+	type name##_WRAPPED(EXPAND b struct _Scope ** _scope) \
 
 #define BOTH(a, b) a b
 #define SECOND(a, b) b
 
-#define GET_ARG_TYPES_AND_NAMES(item, i) () BOTH item
-#define GET_ARG_NAMES(item, i) () SECOND item
+#define ARGS_A(item, i) SECOND item,
+#define ARGS_B(item, i) BOTH item,
+#define ARGS_C(item, i) () BOTH item EXPAND
 
 #define func(type, name, ...) \
 	func_INTERNAL( \
 		type, \
 		name, \
-		(EXPAND MAP((GET_ARG_TYPES_AND_NAMES, COMMA), __VA_ARGS__)), \
-		(EXPAND MAP((GET_ARG_NAMES, COMMA), __VA_ARGS__)))
+		(MAP(ARGS_A, __VA_ARGS__)), \
+		(MAP(ARGS_B, __VA_ARGS__)), \
+		(EXPAND MAP((ARGS_C, COMMA), __VA_ARGS__)()))
 
 #define make(type, name) \
-	struct type * name##_OBJ = malloc(sizeof(struct type))
+	printf(#type " created\n"); \
+	struct type * name##_OBJ = make_obj(malloc(sizeof(struct type)), &type##_DROP, _scope)
 
 #define set(obj, prop, val) \
 	obj##_OBJ->prop = val
@@ -41,11 +71,35 @@ struct TwoVals
 	int b;
 };
 
+void TwoVals_DROP(void * data)
+{
+	printf("TwoVals_DROP()\n");
+	free(data);
+}
+
+struct MyStruct
+{
+	int x, y, z;
+};
+
+void MyStruct_DROP(void * data)
+{
+	printf("MyStruct_DROP()\n");
+	free(data);
+}
+
+func(void, do_nothing)
+{
+	make(MyStruct, aaa);
+}
+
 func(int, add_nums, (int, foo), (int, bar))
 {
 	make(TwoVals, vals);
 	set(vals, a, foo);
+	do_nothing();
 	set(vals, b, bar);
+	make(TwoVals, xyz);
 	return get(vals, a) + get(vals, b);
 }
 
