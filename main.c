1c3ccfc7897be14e16632c5b8513c72505beac72
William W Wold <wilcoze@gmail.com>
2018-03-27 00:50:13 -0400

    it sorta works

--- main.c
+++ main.c
@@ -1,4 +1,39 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "define_EVIL/define_EVIL.h"
+
+#define COMMA() ,
+
+#define func_INTERNAL(type, name, arg_types_and_types, arg_names) \
+	type name##_WRAPPED(EXPAND arg_types_and_types, void *); \
+	type name arg_types_and_types \
+	{ \
+		type ret = name##_WRAPPED(EXPAND arg_names, NULL); \
+		return ret; \
+	} \
+	type name##_WRAPPED(EXPAND arg_types_and_types, void * SCOPE) \
+
+#define BOTH(a, b) a b
+#define SECOND(a, b) b
+
+#define GET_ARG_TYPES_AND_NAMES(item, i) () BOTH item
+#define GET_ARG_NAMES(item, i) () SECOND item
+
+#define func(type, name, ...) \
+	func_INTERNAL( \
+		type, \
+		name, \
+		(EXPAND MAP((GET_ARG_TYPES_AND_NAMES, COMMA), __VA_ARGS__)), \
+		(EXPAND MAP((GET_ARG_NAMES, COMMA), __VA_ARGS__)))
+
+#define make(type, name) \
+	struct type * name##_OBJ = malloc(sizeof(struct type))
+
+#define set(obj, prop, val) \
+	obj##_OBJ->prop = val
+
+#define get(obj, prop) \
+	obj##_OBJ->prop
 
 struct TwoVals
 {
@@ -6,12 +41,12 @@ struct TwoVals
 	int b;
 };
 
-FUNC(int add_nums, (int foo, int bar))
+func(int, add_nums, (int, foo), (int, bar))
 {
-	MAKE(TwoVals, vals);
-	SET(vals, a, foo);
-	SET(vals, b, bar);
-	return GET(vals, a) + GET(vals, b)
+	make(TwoVals, vals);
+	set(vals, a, foo);
+	set(vals, b, bar);
+	return get(vals, a) + get(vals, b);
 }
 
 int main()
