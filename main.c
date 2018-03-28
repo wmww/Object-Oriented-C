57d9b1251bd63240c83fc192766a45c099fde4a0
William W Wold <wilcoze@gmail.com>
2018-03-28 02:52:21 -0400

    tried to change teather logic

--- main.c
+++ main.c
@@ -4,51 +4,65 @@
 #include "define_EVIL/define_EVIL.h"
 
 #define debug EXPAND_TRUE
+#define CHECK EXPAND_TRUE
 //#define debug EXPAND_FALSE;
-
-struct _Scope
-{
-	void * data;
-	void (*drop)(void*);
-	struct _Scope* next;
-};
-
-void * _make_obj(size_t size, void(*drop)(void*), struct _Scope ** scope)
-{
-	void * data = malloc(size);
-	memset(data, 0, size);
-	struct _Scope * tmp = *scope;
-	*scope = malloc(sizeof(struct _Scope));
-	(*scope)->data = data;
-	(*scope)->drop = drop;
-	(*scope)->next = tmp;
-	return data;
-}
-
-void _Scope_drop(struct _Scope * scope)
-{
-	while (scope)
-	{
-		scope->drop(scope->data);
-		scope = scope->next;
-	}
-}
 
 #define _OBJ(name) _##name##_OBJ
 #define _CLASS(name) _##name##_CLASS
+#define _TETHER(name) _##name##_TETHER
 #define _DROP_FUNC(name) _##name##_DROP
 #define _WRAPPED(name) EXPAND_CAT(_, EXPAND_CAT(name, _WRAPPED))
 
+typedef void _CLASS(void);
+
+#define _TETHER_FOR_TYPE(name) \
+	struct _TETHER(name) \
+	{ \
+		struct _CLASS(name) * data; \
+		void (*drop)(void*); \
+		struct _TETHER(void) * next; \
+		struct _TETHER(void) * prev; \
+	}; \
+
+_TETHER_FOR_TYPE(void);
+typedef struct _TETHER(void) _Tether;
+
+void * _make_zeroed_mem(size_t size)
+{
+	void * data = malloc(size);
+	memset(data, 0, size);
+	return data;
+}
+
+void _DROP_FUNC(_Tether)(_Tether * tether)
+{
+	CHECK(if (tether->prev){
+			fprintf(stderr, "WARNING: " TO_STRING(_DROP_FUNC(_Tether)) " called with non-starting tethe\n");
+		})
+	
+	while (tether)
+	{
+		if (tether->drop)
+		{
+			tether->drop(tether->data);
+		}
+		_Tether * next = tether->next;
+		memset(tether, 0, sizeof(_Tether));
+		tether = next;
+	}
+}
+
 #define _func_A(type, name, a, b, c) \
-	type _WRAPPED(name)(EXPAND b struct _Scope **); \
+	type _WRAPPED(name)(EXPAND b _Tether * tether); \
 	type name c \
 	{ \
-		struct _Scope* scope = NULL; \
-		IF(NE(void, type))(type ret =) _WRAPPED(name)(EXPAND a &scope); \
-		_Scope_drop(scope); \
+		_Tether tether; \
+		memset(&tether, 0, sizeof(_Tether)); \
+		IF(NE(void, type))(type ret =) _WRAPPED(name)(EXPAND a &tether); \
+		_DROP_FUNC(_Tether)(&tether); \
 		return IF(NE(void, type))(ret) ; \
 	} \
-	type _WRAPPED(name)(EXPAND b struct _Scope ** _scope) \
+	type _WRAPPED(name)(EXPAND b _Tether * _tether) \
 
 #define BOTH(a, b) a b
 #define SECOND(a, b) b
@@ -65,22 +79,40 @@ void _Scope_drop(struct _Scope * scope)
 		(MAP(ARGS_B, __VA_ARGS__)), \
 		(MAP(ARGS_C, __VA_ARGS__)))
 
-#define make(type, name) \
-	debug(printf(#type " created\n");) \
-	struct _CLASS(type) * _OBJ(name) = _make_obj(sizeof(struct _CLASS(type)), &_DROP_FUNC(type), _scope)
+#define declare(type, name) \
+	struct _TETHER(type) _OBJ(name);
+
+#define make(type) \
+	(struct _TETHER(type)) \
+	{ \
+		.data = _make_zeroed_mem(sizeof(struct _CLASS(type))), \
+		.drop = &_DROP_FUNC(type), \
+		.next = NULL, \
+		.prev = NULL, \
+	} \
+
+#define assign(var, obj) \
+	_OBJ(var) = obj; \
+	_OBJ(var).next = _tether->next; \
+	_OBJ(var).prev = _tether; \
+	_tether->next = (_Tether *)&_OBJ(var); \
+	if (_OBJ(var).next) \
+		_OBJ(var).next->prev = (_Tether *)&_OBJ(var); \
 
 #define set(obj, prop, val) \
-	_##obj##_OBJ->prop = val
+	CHECK(if (!_OBJ(obj).data) { fprintf(stderr, "WARNING: null pointer\n"); }) \
+	_OBJ(obj).data->prop = val
 
 #define get(obj, prop) \
-	_##obj##_OBJ->prop
+	CHECK(_OBJ(obj).data ?) _OBJ(obj).data->prop CHECK(: fprintf(stderr, "WARNING: null pointer\n"))
 
 #define class(name, members) \
 	struct _CLASS(name) \
 	{ \
-		struct _Scope * _scope; \
+		_Tether _tether; \
 		EXPAND members \
 	}; \
+	_TETHER_FOR_TYPE(name);
 
 #define drop(name) \
 	void _WRAPPED(_DROP_FUNC(name))(struct _CLASS(name) * data); \
@@ -88,7 +120,7 @@ void _Scope_drop(struct _Scope * scope)
 	{ \
 		debug(printf("dropping " #name "\n");) \
 		_WRAPPED(_DROP_FUNC(name))(data); \
-		_Scope_drop(((struct _CLASS(name) *)data)->_scope); \
+		_DROP_FUNC(_Tether)(&((struct _CLASS(name) *)data)->_tether); \
 		free(data); \
 	} \
 	void _WRAPPED(_DROP_FUNC(name))(struct _CLASS(name) * data)
@@ -112,16 +144,19 @@ drop(MyStruct) {}
 
 func(void, do_nothing)
 {
-	make(MyStruct, aaa);
+	declare(MyStruct, aaa);
+	assign(aaa, make(MyStruct))
 }
 
 func(int, add_nums, (int, foo), (int, bar))
 {
-	make(TwoVals, vals);
+	declare(TwoVals, vals);
+	assign(vals, make(TwoVals));
 	set(vals, a, foo);
 	do_nothing();
 	set(vals, b, bar);
-	make(TwoVals, xyz);
+	declare(TwoVals, xyz);
+	assign(xyz, make(TwoVals));
 	return get(vals, a) + get(vals, b);
 }
 
